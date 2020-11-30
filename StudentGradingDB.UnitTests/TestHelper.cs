ca0a62c02e240b5b1fba71248da1577780a50259
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-11-30 17:44:08 -0500

    Added helper class for tests

---
+++ StudentGradingDB.UnitTests/TestHelper.cs
@@ -0,0 +1,50 @@
+﻿using DatabaseContext;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentGradingDB.UnitTests
+{
+    /// <summary>
+    ///  Helper class to help with common test configurations options
+    /// </summary>
+  public abstract class TestHelper 
+    {
+        #region Properties
+
+        /// <summary>
+        /// Property to set the SQLite test database
+        /// </summary>
+        protected SqliteConnection Connection { get; set; }
+
+        /// <summary>
+        /// Property to set the context of the database and open it
+        /// </summary>
+        protected StudentGradingContext Context { get; set; }
+
+        #endregion
+
+        /// <summary>
+        ///  Initializes the connection, context and other setup procedures 
+        /// </summary>
+       protected void SetupConnectionAndContext()
+        {
+            Connection = new SqliteConnection("DataSource=:memory:");
+            
+            //create the options
+            var options = new DbContextOptionsBuilder<StudentGradingContext>()
+                       .UseSqlite(Connection)
+                       .Options;
+
+            //set the context
+            Context = new StudentGradingContext(options);
+
+            //Opens the connection
+            Connection.Open();
+
+            Context.Database.EnsureDeleted();
+            Context.Database.EnsureCreated();
+
+        }
+
+    }
+}
