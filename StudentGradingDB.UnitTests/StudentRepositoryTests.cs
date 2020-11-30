202d6af04033a76c25ca420df3fe88c341e128ba
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-11-30 12:45:46 -0500

    Added Initial Tests

--- StudentGradingDB.UnitTests/StudentRepositoryTests.cs
+++ StudentGradingDB.UnitTests/StudentRepositoryTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DatabaseContext;
+using Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositories;
 
 namespace StudentGradingDB.UnitTests
 {
@@ -8,6 +13,40 @@ namespace StudentGradingDB.UnitTests
     [TestClass]
     class StudentRepositoryTests
     {
+
+        [TestMethod]
+        public void AddStudent()
+        {
+            // In-memory database only exists while the connection is open
+            var connection = new SqliteConnection("DataSource=:memory:");
+            
+            //open the connection 
+            connection.Open();
+
+            try 
+            {
+                var options = new DbContextOptionsBuilder<StudentGradingContext>()
+                       .UseSqlite(connection)
+                       .Options;
+
+                //create the context class
+                var context = new StudentGradingContext();
+                var studentRepository = new StudentRepository(context);
+
+                var newStudent = new Student()
+                {
+                    FirstName = "Mikesh",
+                    LastName = "Mistry"
+                };
+
+            }
+            finally 
+            {
+
+                //close the connection
+                connection.Close();
+            }
+        }
 
     }
 }
