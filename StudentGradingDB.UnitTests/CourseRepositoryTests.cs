55e6396f273045dfcc9650eab86a62ec02db7013
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-11-30 22:44:20 -0500

    Completed all unit tests for course repository

--- StudentGradingDB.UnitTests/CourseRepositoryTests.cs
+++ StudentGradingDB.UnitTests/CourseRepositoryTests.cs
@@ -174,11 +174,265 @@ namespace StudentGradingDB.UnitTests
                 //count should be zero
                 Assert.AreEqual(0, courseListAfterRemoved.Count());
             }
-            
+
 
         #endregion
 
-        #region Other Query Tests
+   #region Other Query Tests
+
+        #region  AssignTeacherToCourse Tests
+
+        /// <summary>
+        /// Test to assign a course to a teacher that does not exist
+        /// </summary>
+        [TestMethod]
+        public void AssignTeachToCourse_TeacherNotFound_ReturnsFalse()
+        {
+            var courseRepository = new CourseRepository(Context);
+
+            //create the course
+            var course = new Course()
+            {
+                Name = "Introduction to C#",
+                Description = "Introduction to C# programming fundamentals"
+            };
+
+            //add a course
+            courseRepository.Add(course);
+
+            //find the newly added course
+            var enrollingCourse = courseRepository.Find(course => course.Name == "Introduction to C#")
+                                                  .FirstOrDefault();
+            //found the course
+            if (enrollingCourse != null)
+            {
+                //assign a course to a teacher that does not exist
+                var assignedTeacherToCourse = courseRepository.AssignTeacherToCourse(1234, enrollingCourse.CourseId);
+
+                //should return false indicating course was not assigned to the teacher
+                Assert.IsFalse(assignedTeacherToCourse);
+            }
+        }
+
+        /// <summary>
+        /// Test to assign a teacher to a course that does not exist
+        /// </summary>
+        [TestMethod]
+        public void AssignTeachToCourse_CourseNotFound_ReturnsFalse()
+        {
+            var teacherRepository = new TeacherRepository(Context);
+
+            var teacher = new Teacher()
+            {
+                FirstName = "Mikesh",
+                LastName = "Mistry"
+            };
+
+            //add a teacher
+            teacherRepository.Add(teacher);
+
+            //find the newly added teacher
+            var enrollingTeacher = teacherRepository.Find(teacher => teacher.FirstName == "Mikesh")
+                                                  .FirstOrDefault();
+            //found the teacher
+            if (enrollingTeacher != null)
+            {
+                //assign a teacher to a course that does not exist
+                var assignedCourseToTeacher = courseRepository.AssignTeacherToCourse(enrollingTeacher.TeacherId, 1234);
+
+                //should return false indicating course was not assigned to the teacher
+                Assert.IsFalse(assignedCourseToTeacher);
+            }
+        }
+
+        /// <summary>
+        /// Test to assign a teacher to a course where both the teacher and course are not found
+        /// </summary>
+        [TestMethod]
+        public void AssignTeachToCourse_TeacherNotFoundAndCourseNotFound_ReturnsFalse()
+        {
+           
+
+                //assign a teacher to a course where both teacher and course are not found
+                var assignedCourseToTeacher = courseRepository.AssignTeacherToCourse(64587, 1234);
+
+                //should return false indicating course was not assigned to the teacher
+                Assert.IsFalse(assignedCourseToTeacher);
+         }
+
+        /// <summary>
+        /// Test to assign a teacher to a course successfully returns true
+        /// </summary>
+        [TestMethod]
+        public void AssignTeacherToCourse_TeacherAndCourseFound_ReturnsTrue()
+        {
+            var teacherRepository = new TeacherRepository(Context);
+
+            var teacher = new Teacher()
+            {
+                FirstName = "Mikesh",
+                LastName = "Mistry"
+            };
+
+            //add a teacher
+            teacherRepository.Add(teacher);
+
+            //create the course
+            var course = new Course()
+            {
+                Name = "Introduction to C#",
+                Description = "Introduction to C# programming fundamentals"
+            };
+
+            //add a course
+            courseRepository.Add(course);
+
+            //find the newly added teacher
+            var enrollingTeacher = teacherRepository.Find(teacher => teacher.FirstName == "Mikesh")
+                                                  .FirstOrDefault();
+
+            //find the newly added course
+            var enrollingCourse = courseRepository.Find(course => course.Name == "Introduction to C#")
+                                                  .FirstOrDefault();
+            //teacher and course found
+            if(enrollingTeacher !=null && enrollingCourse !=null)
+            {
+                var isEnrolled = courseRepository.AssignTeacherToCourse(enrollingTeacher.TeacherId, enrollingCourse.CourseId);
+                Assert.IsTrue(isEnrolled);
+
+                Assert.AreEqual(1, enrollingTeacher.Courses.Count());
+            }
+        }
+
+        #endregion
+
+        #region EnrollStudentIntoCourse
+       
+        /// <summary>
+        /// Test to enroll a student into a course where the student does not exist
+        /// </summary>
+        [TestMethod]
+        public void EnrollStudentIntoCourse_StudentNotFound_ReturnsFalse()
+        {
+            var courseRepository = new CourseRepository(Context);
+
+            //create the course
+            var course = new Course()
+            {
+                Name = "Introduction to C#",
+                Description = "Introduction to C# programming fundamentals"
+            };
+
+            //add a course
+            courseRepository.Add(course);
+
+            //find the newly added course
+            var enrollingCourse = courseRepository.Find(course => course.Name == "Introduction to C#")
+                                                  .FirstOrDefault();
+            //found the course
+            if (enrollingCourse != null)
+            {
+                //assign a course to a student that does not exist
+                var assignedStudentToCourse = courseRepository.EnrollStudentIntoCourse(1234, enrollingCourse.CourseId);
+
+                //should return false indicating course was not assigned to the student
+                Assert.IsFalse(assignedStudentToCourse);
+            }
+        }
+
+
+        /// <summary>
+        /// Test to enroll a student to a course that does not exist
+        /// </summary>
+        [TestMethod]
+        public void EnrollStudentIntoCourse_CourseNotFound_ReturnsFalse()
+        {
+            var studentRepository = new StudentRepository(Context);
+
+            var student = new Student()
+            {
+                FirstName = "Mikesh",
+                LastName = "Mistry"
+            };
+
+            //add a student
+            studentRepository.Add(student);
+
+            //find the newly added teacher
+            var enrollingStudent = studentRepository.Find(student => student.FirstName == "Mikesh")
+                                                  .FirstOrDefault();
+            //found the student
+            if (enrollingStudent != null)
+            {
+                //assign a student to a course that does not exist
+                var enrolledStudent = courseRepository.EnrollStudentIntoCourse(enrollingStudent.StudentId, 1234);
+
+                //should return false indicating student was not enrolled in the course
+                Assert.IsFalse(enrolledStudent);
+            }
+        }
+
+
+        /// <summary>
+        /// Test to enroll a student to a course where both the student and course are not found
+        /// </summary>
+        [TestMethod]
+        public void EnrollStudentIntoCourse_StudentNotFoundAndCourseNotFound_ReturnsFalse()
+        {
+
+            //assign a teacher to a course where both teacher and course are not found
+            var enrollCourseToStudent = courseRepository.EnrollStudentIntoCourse(64587, 1234);
+
+            //should return false indicating course was not assigned to the teacher
+            Assert.IsFalse(enrollCourseToStudent);
+        }
+
+
+        /// <summary>
+        /// Test to enroll a student to a course successfully returns true
+        /// </summary>
+        [TestMethod]
+        public void EnrollStudentIntoCourse_StudentAndCourseFound_ReturnsTrue()
+        {
+            var studentRepository = new StudentRepository(Context);
+
+            var student = new Student()
+            {
+                FirstName = "Mikesh",
+                LastName = "Mistry"
+            };
+
+            //add a student
+            studentRepository.Add(student);
+
+            //create the course
+            var course = new Course()
+            {
+                Name = "Introduction to C#",
+                Description = "Introduction to C# programming fundamentals"
+            };
+
+            //add a course
+            courseRepository.Add(course);
+
+            //find the newly added student
+            var enrollingStudent = studentRepository.Find(teacher => teacher.FirstName == "Mikesh")
+                                                  .FirstOrDefault();
+
+            //find the newly added course
+            var enrollingCourse = courseRepository.Find(course => course.Name == "Introduction to C#")
+                                                  .FirstOrDefault();
+            //student and course found
+            if (enrollingStudent != null && enrollingCourse != null)
+            {
+                var isEnrolled = courseRepository.EnrollStudentIntoCourse(enrollingStudent.StudentId, enrollingCourse.CourseId);
+                Assert.IsTrue(isEnrolled);
+
+                Assert.AreEqual(1, enrollingStudent.Courses.Count());
+            }
+        }
+
+        #endregion
 
         #endregion
 
