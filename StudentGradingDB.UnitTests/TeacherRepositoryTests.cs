66c3ef8c34d07033c537a2af8619056c287d3e26
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-11-30 23:09:44 -0500

    Completed unit tests for teacher repository

--- StudentGradingDB.UnitTests/TeacherRepositoryTests.cs
+++ StudentGradingDB.UnitTests/TeacherRepositoryTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Repositories;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StudentGradingDB.UnitTests
@@ -170,10 +171,89 @@ namespace StudentGradingDB.UnitTests
                 //count should be zero
                 Assert.AreEqual(0, teacherListAfterRemoved.Count());
             }
-         
+
         #endregion
 
         #region Other Query Tests
+
+        #region GetAssignedCourse Tests
+        /// <summary>
+        /// Test to get courses assigned to a teacher that does not exists
+        /// </summary>
+        [TestMethod]
+        public void GetAssignedCourse_TeacherNotFound_ReturnsFalse()
+        {
+            var coursesList = teacherRepository.GetAssignedCourses(1223);
+            //should return a null course list
+            Assert.IsNull(coursesList);
+
+        }
+
+
+        /// <summary>
+        /// Test to get all enrolled courses for a teacher
+        /// </summary>
+        [TestMethod]
+        public void GetAssignedCourse_TeacherFound_ReturnsCourses()
+        {
+            //create the new student
+            var newTeacher = new Teacher()
+            {
+                FirstName = "Mikesh",
+                LastName = "Mistry"
+            };
+
+            //add the teacher to the database
+            teacherRepository.Add(newTeacher);
+
+            //add some courses
+            var courseRepository = new CourseRepository(Context);
+
+            var courseList = new List<Course>();
+
+            // add a list of courses
+            courseList.Add(new Course { Name = "Introduction to C#", Description = "Introduces students to C# programming" });
+            courseList.Add(new Course { Name = "Introduction to Java", Description = "Introduces students to Java programming" });
+
+            //add the course using add range
+            courseRepository.AddRange(courseList);
+
+            //use the find method to find a teacher 
+            var foundTeacher = teacherRepository.Find(teacher => teacher.FirstName == "Mikesh" && teacher.LastName == "Mistry").FirstOrDefault();
+
+            //found the student
+            if (foundTeacher != null)
+            {
+                //enroll the teacher into the two course
+                foreach (var course in courseList)
+                {
+                    courseRepository.AssignTeacherToCourse(foundTeacher.TeacherId, course.CourseId);
+                }
+
+
+                //get the found teacher
+                var teacherCourseList = teacherRepository.GetId(foundTeacher.TeacherId);
+
+                if (teacherCourseList != null)
+                {
+                    //the count of the courseList used to add the courses to the teacher
+                    var courseListCount = courseList.Count();
+                    var teacherEnrolledCoursesCount = teacherCourseList.Courses.Count();
+
+                    //insure the counts are the same
+                    Assert.AreEqual(courseListCount, teacherEnrolledCoursesCount);
+
+                    //insure that ids are the same
+                    Assert.AreEqual(foundTeacher.TeacherId, teacherCourseList.TeacherId);
+
+                }
+
+
+            }
+
+        }
+
+        #endregion
 
         #endregion
     }
